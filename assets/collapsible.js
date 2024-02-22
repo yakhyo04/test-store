d380b00ea997cf28fa2491252467e0560401f4ec
Yakhyo Abdulkhakov <fenixrelaxation@gmail.com>
2024-02-22 20:11:06 +0500

    added header-topbar and header2

--- assets/collapsible.js
+++ assets/collapsible.js
@@ -1,17 +1,84 @@
-function toggleAccordion(accordionItemHeader) {
-    accordionItemHeader.classList.toggle("active");
-    const accordionItem = accordionItemHeader.closest(".accordion-item");
-    const accordionItemBody = accordionItem.querySelector(".accordion-item-body");
-    if (accordionItemHeader.classList.contains("active")) {
-      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-    } else {
-      accordionItemBody.style.maxHeight = 0;
-    }
-  }
+// function toggleAccordion(accordionItem) {
+//     accordionItem.classList.toggle("active");
+// }
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const accordionItems = document.querySelectorAll(".accordion-item");
+//     accordionItems.forEach(accordionItem => {
+//         const accordionItemHeader = accordionItem.querySelector(".accordion-item-header");
+//         accordionItemHeader.addEventListener("click", () => {
+//             toggleAccordion(accordionItem);
+//         });
+//     });
+// });
+
+// class Accordion {
+//     constructor(accordionItem, onToggle) {
+//       this.accordionItem = accordionItem;
+//       this.headerElement = accordionItem.querySelector("[data-component='header']");
+//       this.bodyElement = accordionItem.querySelector("[data-body='header']");
+//       this.onToggle = onToggle || (() => {}); 
+//       this.initializeAccordion();
+//     }
   
-  const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
-  accordionItemHeaders.forEach(accordionItemHeader => {
-    accordionItemHeader.addEventListener("click", () => {
-      toggleAccordion(accordionItemHeader);
+//     toggleAccordion() {
+//       this.accordionItem.classList.toggle("active");
+//       this.onToggle(this.accordionItem);
+//     }
+  
+//     initializeAccordion() {
+//       this.headerElement.addEventListener("click", () => {
+//         this.toggleAccordion();
+//       });
+//     }
+//   }
+  
+//   document.addEventListener("DOMContentLoaded", function () {
+//     const accordionItems = document.querySelectorAll("expended-accordion");
+//     accordionItems.forEach(accordionItem => {
+//       new Accordion(accordionItem, accordionItem => {
+//         console.log("Accordion item:", accordionItem);
+//       });
+//     });
+//   });
+
+// const onDocumentReady = (callback) => (document.readyState === "complete" || document.readyState === "interactive"
+//   ? setTimeout(callback, 1)
+//   : document.addEventListener("DOMContentLoaded", callback));
+  
+
+class Collapsable extends HTMLElement{
+  constructor(){
+    super()
+  }
+
+  connectedCallback(){
+    onDocumentReady(() => {
+        this.headerElement = this.querySelector("[data-component='header']"); 
+        this.bodyElement = this.querySelector("[data-body='header']");
+        console.log("initialized");
+        this.init();
+    })
+  }
+
+  init(){
+    this.headerElement.addEventListener("click", () => {
+      console.log("clicked");
+      this.toggle();
     });
-  });
+  }
+
+  toggle(){
+    this.classList.toggle("active");
+  }
+
+  disconnectedCallback() {
+    console.log('Disconnected Callback');
+  }
+
+  adoptedCallback() {
+    console.log('Adopted Callback');
+  }
+}
+
+customElements.define('expended-accordion', Collapsable)
