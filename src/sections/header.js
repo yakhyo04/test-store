afc53f43162b720e7064511558a4b6115ad986af
Yakhyo Abdulkhakov <fenixrelaxation@gmail.com>
2024-03-04 17:03:19 +0500

    happened some changes in header, header-topbar and another files

--- src/sections/header.js
+++ src/sections/header.js
@@ -1,1 +1,25 @@
 import './styles/header.scss';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const iconHamburger = document.querySelector(".header__icon--hamburger");
+    const mobileDrawer = document.querySelector(".header__mobile--drawer");
+    const iconClose = document.querySelector(".header__icon--close");
+    const bodyWrapper = document.querySelector("body");
+    const headerOverlay = document.querySelector(".header__mobile--overlay");
+
+    const openMobileDrawer = () => {
+        mobileDrawer.classList.add("open");
+        headerOverlay.classList.add("active");
+        bodyWrapper.style.overflow = 'hidden';
+    };
+
+    const closeMobileDrawer = () => {
+        mobileDrawer.classList.remove("open");
+        headerOverlay.classList.remove("active");
+        bodyWrapper.style.overflow = 'visible';
+    };
+
+    iconHamburger.addEventListener("click", openMobileDrawer);
+    iconClose.addEventListener("click", closeMobileDrawer);
+    headerOverlay.addEventListener("click", closeMobileDrawer);
+});
