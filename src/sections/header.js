954618139506edbeda42d0699f7b5a6932e634a2
Yakhyo Abdulkhakov <fenixrelaxation@gmail.com>
2024-03-04 18:11:05 +0500

    added header search form popup

--- src/sections/header.js
+++ src/sections/header.js
@@ -6,6 +6,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const iconClose = document.querySelector(".header__icon--close");
     const bodyWrapper = document.querySelector("body");
     const headerOverlay = document.querySelector(".header__mobile--overlay");
+    const headerSearch = document.querySelector(".header__search");
+    const headerSearchWrapper = document.querySelector(".header__search--wrapper");
+    const headerClose = document.querySelector(".header__search--close");
 
     const openMobileDrawer = () => {
         mobileDrawer.classList.add("open");
@@ -17,9 +20,18 @@ document.addEventListener("DOMContentLoaded", () => {
         mobileDrawer.classList.remove("open");
         headerOverlay.classList.remove("active");
         bodyWrapper.style.overflow = 'visible';
+        headerSearchWrapper.classList.remove("active");
     };
+
+    const openHeaderSearch = () => {
+        headerSearchWrapper.classList.add("active");
+        headerOverlay.classList.add("active");
+        bodyWrapper.style.overflow = 'hidden';
+    }
 
     iconHamburger.addEventListener("click", openMobileDrawer);
     iconClose.addEventListener("click", closeMobileDrawer);
     headerOverlay.addEventListener("click", closeMobileDrawer);
+    headerSearch.addEventListener("click", openHeaderSearch);
+    headerClose.addEventListener("click", closeMobileDrawer);
 });
