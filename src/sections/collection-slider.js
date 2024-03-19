6162e90757634e5f8b38d3f4acb4d0961ad21b2c
Yakhyo Abdulkhakov <fenixrelaxation@gmail.com>
2024-03-20 01:37:48 +0500

    feat: add to cart

--- src/sections/collection-slider.js
+++ src/sections/collection-slider.js
@@ -1,1 +1,37 @@
-import './styles/collection-slider.scss';
+import './styles/collection-slider.scss'
+
+let featuredButton = document.querySelectorAll(".featured__button");
+let addToCartForm = document.querySelectorAll('form[action$="/cart/add"]');
+
+featuredButton.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        let productId;
+        addToCartForm.forEach((formItem) => {
+            productId = formItem.childNodes[1].defaultValue
+        })
+
+        let formData = {
+            'items': [{
+                'id': productId,
+                'quantity': 1
+            }]
+        }
+
+        console.log(featuredButton[0].id);
+
+        fetch(window.Shopify.routes.root + 'cart/add.js', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+        })
+    })
+})
